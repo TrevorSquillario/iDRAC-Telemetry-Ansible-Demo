6a00202768962992e62d9b95b6b4ab13894d867b
Trevor Squillario <Trevor_Squillario@Dell.com>
2024-06-14 15:04:32 -0600

    Add prometheus pump

---
+++ docker-compose/cmd/prometheuspump/prometheuspump.go
@@ -0,0 +1,180 @@
+// Licensed to You under the Apache License, Version 2.0.
+
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+
+	"github.com/dell/iDRAC-Telemetry-Reference-Tools/internal/databus"
+	"github.com/dell/iDRAC-Telemetry-Reference-Tools/internal/messagebus/stomp"
+)
+
+var configStrings = map[string]string{
+	"mbhost": "activemq",
+	"mbport": "61613",
+}
+
+var collectors map[string]map[string]*prometheus.GaugeVec
+
+func getValidMetricName(name string) string {
+	// Metric names can contain letters, numbers, underscores, or colons, based on the regular expression [a-zA-Z_:][a-zA-Z0-9_:]*.
+	// Reserve colons (:) in the name for calculated or aggregated metrics, such as those produced by rollup rules.
+
+	// Rename Context 'SMARTData:Disk.Bay.0:Enclosure.Internal.0-1:RAID.SL.3-1' to 'SMARTData'
+	colon_match := strings.Index(name, ":")
+	var new_name string
+	if colon_match > -1 {
+		new_name = name[:colon_match]
+	} else {
+		// Rename Context like 'DIMM.Socket.A2' to 'DIMM'
+		period_match := strings.Index(name, ".")
+		if period_match > -1 {
+			new_name = name[:period_match]
+		} else {
+			new_name = name
+		}
+	}
+	log.Printf("Renaming Metric from %s to %s at string index %s", name, new_name, colon_match)
+	return new_name
+}
+
+func doFQDDGuage(value databus.DataValue, registry *prometheus.Registry, hostTags string, hostName string) {
+	log.Printf("DEBUG: doFQDDGuage value %#v\n", value)
+	if collectors["FQDD"] == nil {
+		collectors["FQDD"] = make(map[string]*prometheus.GaugeVec)
+	}
+	if collectors["FQDD"][value.ID] == nil {
+		guage := prometheus.NewGaugeVec(
+			prometheus.GaugeOpts{
+				Namespace: "PowerEdge",
+				Subsystem: getValidMetricName(value.Context),
+				Name:      value.ID,
+			},
+			[]string{
+				"ServiceTag",
+				"FQDD",
+				"HostTags",
+				"HostName",
+			})
+		registry.MustRegister(guage)
+		floatVal, err := strconv.ParseFloat(value.Value, 64)
+		if err != nil {
+			if value.Value == "Up" || value.Value == "Operational" {
+				floatVal = 1
+			}
+		}
+		guage.WithLabelValues(value.System, value.Context, hostTags, hostName).Set(floatVal)
+		collectors["FQDD"][value.ID] = guage
+	} else {
+		guage := collectors["FQDD"][value.ID]
+		floatVal, err := strconv.ParseFloat(value.Value, 64)
+		if err != nil {
+			if value.Value == "Up" || value.Value == "Operational" {
+				floatVal = 1
+			}
+		}
+		guage.WithLabelValues(value.System, value.Context, hostTags, hostName).Set(floatVal)
+	}
+}
+
+func doNonFQDDGuage(value databus.DataValue, registry *prometheus.Registry, hostTags string, hostName string) {
+	log.Printf("DEBUG: doNonFQDDGuage value %#v\n", value)
+	value.Context = strings.Replace(value.Context, " ", "", -1)
+	if collectors[value.Context] == nil {
+		collectors[value.Context] = make(map[string]*prometheus.GaugeVec)
+	}
+	if collectors[value.Context][value.ID] == nil {
+		guage := prometheus.NewGaugeVec(
+			prometheus.GaugeOpts{
+				Namespace: "PowerEdge",
+				Subsystem: value.Context,
+				Name:      value.ID,
+			},
+			[]string{
+				"ServiceTag",
+				"FQDD",
+				"HostTags",
+				"HostName",
+			})
+		registry.MustRegister(guage)
+		floatVal, _ := strconv.ParseFloat(value.Value, 64)
+		guage.WithLabelValues(value.System, value.Context, hostTags, hostName).Set(floatVal)
+		collectors[value.Context][value.ID] = guage
+	} else {
+		guage := collectors[value.Context][value.ID]
+		floatVal, _ := strconv.ParseFloat(value.Value, 64)
+		guage.WithLabelValues(value.System, value.Context, hostTags, hostName).Set(floatVal)
+	}
+}
+
+func handleGroups(groupsChan chan *databus.DataGroup, registry *prometheus.Registry) {
+	collectors = make(map[string]map[string]*prometheus.GaugeVec)
+	for {
+		group := <-groupsChan
+		for _, value := range group.Values {
+			//log.Print("value: ", value)
+			if group.Label == "Redfish Metric Report" {
+				if strings.Contains(value.Context, ".") {
+					doFQDDGuage(value, registry, group.HostTags, group.HostName)
+				} else {
+					doNonFQDDGuage(value, registry, group.HostTags, group.HostName)
+				}
+			}
+		}
+	}
+}
+
+func getEnvSettings() {
+	mbHost := os.Getenv("MESSAGEBUS_HOST")
+	if len(mbHost) > 0 {
+		configStrings["mbhost"] = mbHost
+	}
+	mbPort := os.Getenv("MESSAGEBUS_PORT")
+	if len(mbPort) > 0 {
+		configStrings["mbport"] = mbPort
+	}
+}
+
+func main() {
+
+	//Gather configuration from environment variables
+	getEnvSettings()
+
+	dbClient := new(databus.DataBusClient)
+	//Initialize messagebus
+	for {
+		stompPort, _ := strconv.Atoi(configStrings["mbport"])
+		mb, err := stomp.NewStompMessageBus(configStrings["mbhost"], stompPort)
+		if err != nil {
+			log.Printf("Could not connect to message bus: %s", err)
+			time.Sleep(5 * time.Second)
+		} else {
+			dbClient.Bus = mb
+			defer mb.Close()
+			break
+		}
+	}
+
+	groupsIn := make(chan *databus.DataGroup, 10)
+	dbClient.Subscribe("/prometheus")
+	dbClient.Get("/prometheus")
+	go dbClient.GetGroup(groupsIn, "/prometheus")
+
+	registry := prometheus.NewRegistry()
+	go handleGroups(groupsIn, registry)
+
+	gatherer := prometheus.Gatherer(registry)
+	http.Handle("/metrics", promhttp.HandlerFor(gatherer, promhttp.HandlerOpts{}))
+	err := http.ListenAndServe(":2112", nil)
+	if err != nil {
+		log.Printf("Failed to start webserver %v", err)
+	}
+}
